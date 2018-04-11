6da3f20accda9acd1ab188e1dd525bf30730cf5e
3dtuning <ashenemy@gmail.com>
2018-04-11 15:02:42 +0400

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 console.log('test');
 console.log('test');
+console.log('test');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
